0754338c0d2f40e5f23974026fa5174536f1caec
dseeg <85651651+dseeg@users.noreply.github.com>
2021-06-25 23:17:53 -0400

    added connection string from user secrets. closes #1

--- IoT-Environment/Startup.cs
+++ IoT-Environment/Startup.cs
@@ -1,6 +1,8 @@
+using IoT_Environment.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +22,9 @@ namespace IoT_Environment
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<IoTContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:IoT"]));
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
