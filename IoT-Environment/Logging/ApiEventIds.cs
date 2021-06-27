454d160fa7ce6b2968c8c786164d1a28d33884d3
dseeg <85651651+dseeg@users.noreply.github.com>
2021-06-27 10:22:33 -0400

    started building logging event ids

---
+++ IoT-Environment/Logging/ApiEventIds.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoT_Environment.Logging
+{
+    public class ApiEventIds
+    {
+        public const int ReadAllReports = 1000;
+        public const int ReadReport     = 1001;
+        public const int UpdateReport   = 1002;
+        public const int CreateReport   = 1003;
+        public const int DeleteReport   = 1004;
+
+        public const int ReadReportNotFound   = 2000;
+        public const int UpdateReportNotFound = 2002;
+        public const int DeleteReportNotFound = 2003;
+
+        public const int UnknownException = 0;
+    }
+}
