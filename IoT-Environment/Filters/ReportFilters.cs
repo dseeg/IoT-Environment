6e3ef7f8ff5dc5cd64586b4f1a65450a55110781
dseeg <85651651+dseeg@users.noreply.github.com>
2021-06-26 22:34:40 -0400

    adding filters for pulling report

---
+++ IoT-Environment/Filters/ReportFilters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoT_Environment.Filters
+{
+    public class ReportFilters
+    {
+        public decimal LastMinutes { get; set; } = 5;
+        public string DataType { get; set; }
+    }
+}
