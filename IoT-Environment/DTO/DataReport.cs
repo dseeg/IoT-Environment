3d11fd5048bd74b3076f5cf366cc19fd6bcd92d2
dseeg <85651651+dseeg@users.noreply.github.com>
2021-06-26 20:10:53 -0400

    initial DTO structure

---
+++ IoT-Environment/DTO/DataReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoT_Environment.DTO
+{
+    public class DataReport
+    {
+        public DateTime PostedOn { get; set; }
+        public decimal Value { get; set; }
+        public string DataType { get; set; }
+        public string DataUnits { get; set; }
+        public string RelayName { get; set; }
+        public string DeviceName { get; set; }
+        public string DeviceType { get; set; }
+    }
+}
---
+++ IoT-Environment/DTO/DeviceData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoT_Environment.DTO
+{
+    public class DeviceData
+    {
+        public string RelayPhysicalAddress { get; set; }
+        public string RelayNetworkAddress { get; set; }
+        public string DeviceAddress { get; set; }
+        public int DataType { get; set; }
+        public decimal Value { get; set; }
+    }
+}
