889ad61916eb23e43bd8f6ad4e504f6d5ec9a72b
dseeg <85651651+dseeg@users.noreply.github.com>
2021-06-27 10:50:19 -0400

    more logging event id changes

--- IoT-Environment/Logging/ApiEventIds.cs
+++ IoT-Environment/Logging/ApiEventIds.cs
@@ -17,15 +17,21 @@ namespace IoT_Environment.Logging
 
         public const int ReadAllRelays  = 1010;
         public const int ReadRelay      = 1011;
-        public const int UpdateRelays   = 1012;
-        public const int CreateRelays   = 1013;
-        public const int DeleteRelays   = 1014;
+        public const int UpdateRelay    = 1012;
+        public const int CreateRelay    = 1013;
+        public const int DeleteRelay    = 1014;
 
         public const int ReadAllDevices = 1020;
         public const int ReadDevice     = 1021;
         public const int UpdateDevice   = 1022;
         public const int CreateDevice   = 1023;
         public const int DeleteDevice   = 1024;
+
+        public const int ReadAllDataTypes   = 1030;
+        public const int ReadDataType       = 1031;
+        public const int UpdateDataType     = 1032;
+        public const int CreateDataType     = 1033;
+        public const int DeleteDataType     = 1034;
 
         public const int ReadReportNotFound     = 2000;
         public const int UpdateReportNotFound   = 2002;
