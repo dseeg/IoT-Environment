2471d8ab0698eea7b3f43d94cc3afff7a62571d9
dseeg <85651651+dseeg@users.noreply.github.com>
2021-06-27 22:09:37 -0400

    initial device controller implementation. towards #6

---
+++ IoT-Environment/DTO/DeviceRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoT_Environment.DTO
+{
+    public class DeviceRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string ConnectionType { get; set; }
+        public string RelayPhysicalAddress { get; set; }
+        public string RelayNetworkAddress { get; set; }
+    }
+}
