5bc98511808292affbe75c7056ff100a51ee664b
dseeg <85651651+dseeg@users.noreply.github.com>
2021-06-28 21:10:09 -0400

    simple extension method to update network address if its different

---
+++ IoT-Environment/Extensions/RelayExtension.cs
@@ -0,0 +1,24 @@
+﻿using IoT_Environment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace IoT_Environment.Extensions
+{
+    public static class RelayExtension
+    { 
+        public static bool TryUpdateNetworkAddress(this Relay relay, string networkAddr)
+        {
+            if (relay.NetworkAddress != networkAddr)
+            {
+                relay.NetworkAddress = networkAddr;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
