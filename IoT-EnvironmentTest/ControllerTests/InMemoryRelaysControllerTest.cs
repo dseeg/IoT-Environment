69fb3f9a06625339e4bf72c8fef9b370a571c372
dseeg <85651651+dseeg@users.noreply.github.com>
2021-07-01 22:23:45 -0400

    basis for API unit tests going forward

---
+++ IoT-EnvironmentTest/ControllerTests/InMemoryRelaysControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using IoT_Environment.Controllers;
+using IoT_Environment.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+
+namespace IoT_EnvironmentTest.ControllerTests
+{
+    public class InMemoryRelaysControllerTest : RelaysControllerTest
+    {
+        public InMemoryRelaysControllerTest()
+            : base(
+                new DbContextOptionsBuilder<IoTContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options)
+        {
+        }
+
+        [Fact]
+        public async void Can_Get_Relays()
+        {
+            using var context = new IoTContext(ContextOptions);
+            var controller = new RelaysController(context, NullLogger<RelaysController>.Instance);
+
+            var actionResult = await controller.GetRelays();
+
+            Assert.NotEmpty(actionResult.Value);
+        }
+
+        [Fact]
+        public async void Can_Get_Relay()
+        {
+            using var context = new IoTContext(ContextOptions);
+            var controller = new RelaysController(context, NullLogger<RelaysController>.Instance);
+
+            var actionResult = await controller.GetRelay(1);
+
+            Assert.NotNull(actionResult.Value);
+            Assert.Equal(1, actionResult.Value.Id);
+        }
+    }
+}
--- IoT-EnvironmentTest/ControllerTests/RelaysControllerTest.cs
+++ IoT-EnvironmentTest/ControllerTests/RelaysControllerTest.cs
@@ -1,14 +1,40 @@
+using IoT_Environment.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
-using Xunit;
 
-namespace IoT_EnvironmentTest
+namespace IoT_EnvironmentTest.ControllerTests
 {
     public class RelaysControllerTest
     {
-        [Fact]
-        public void Test1()
-        {
+        protected DbContextOptions<IoTContext> ContextOptions { get; }
 
+        protected RelaysControllerTest(DbContextOptions<IoTContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+
+            Seed();
+        }
+
+        private void Seed()
+        {
+            using var context = new IoTContext(ContextOptions);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            Relay relay = new()
+            {
+                DateRegistered = new DateTime(2021, 7, 1),
+                Description = "Relay description",
+                Name = "Relay name",
+                PhysicalAddress = "A1:B2:C3:D4:E5:F6",
+                NetworkAddress = "127.0.0.1",
+                Stale = false,
+            };
+
+            context.Add(relay);
+
+            context.SaveChanges();
         }
     }
 }
